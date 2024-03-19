8d217e83ab40eff018f978038ff886abb584b417
Cali <101529632+BobbyJr9223@users.noreply.github.com>
2024-03-19 14:28:23 +0000

    https://lm.facebook.com/l.php?u=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fauth%3Fclient_id%3D15057814354-80cg059cn49j6kmhhkjam4b00on1gb2n.apps.googleusercontent.com%26state%3DAReRRJwbbuAhElKtTB_9BCpHzvJtI1l3ce-m4QRaNKpoJo_KMmwW28O54O9wLg-b__6U6cDE-1aSIcX4yaWWkQZ0RLWIAhxKeVIlS2-yeje_XGzHj-Bz6QUV6NvLMlKzwz4taMCiG4y0B7i3z7SuDXO7s-4ViHIotsUFLLlhwrqOpAlvuN-U9BiMApbG-vEFUW0-pCst3Ccz5vgyZ5QbDeXo2t6r9_pQoM6Ynvof-3X7bA9j7mgXpNCawl0g8fK4ME7S8rO-zqWGnzBy_zDBcLSo%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fm.facebook.com%252Foauth2%252Fredirect%252F%26scope%3Dopenid%2Bemail%26login_hint%3Dcali.reynolds0767%2540gmail.com&h=AT1bV-E0OTsP5oFBgcjsgFINP9ydvZbjpPkCbJASdC_XCjc1eDBAB4Ppp8L7kYRts9PBOCubvv4EdDl0J50K8vPZPmIlYQbDgg5T_4ByQ1wpQCdmXHtqirsBajFT77hCCkyoolb5TP3HlVjGa5SWlJTx1IVSWhM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+W➜ /workspace git:(facebook) ✗ ➜ /workspace git:(facebook) ✗ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
